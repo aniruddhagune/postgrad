64ab90ca221ec7dc64d3824f6c8213c7f5cdc7f6
Aniruddha <130901487+aniruddhagune@users.noreply.github.com>
2025-04-03 22:07:21 +0530

    Update Week V & VI.sql

--- SemII/DBMS/Week V & VI.sql
+++ SemII/DBMS/Week V & VI.sql
@@ -70,3 +70,115 @@ GROUP BY bname;
 SELECT DISTINCT(c.city) AS City, SUM(d.amount) AS Amount FROM customer c
 JOIN deposit d ON d.cname = c.cname
 GROUP by city;
+
+---- 13. Give city wise name and branch wise deposit.
+
+SELECT b.city, d.bname, SUM(d.amount) AS total_deposit FROM deposit d
+JOIN branch b ON b.bname = d.bname
+GROUP BY b.city, d.bname;
+
+---- 14. Give the branch wise deposit of customer after account date 1-jan-2006.
+
+-- Branch wise total deposits.
+SELECT bname, SUM(amount) AS `Total Deposit` FROM deposit
+WHERE STR_TO_DATE(adate, '%d-%b-%Y') > '2006-01-06'
+GROUP BY bname ORDER BY bname;
+
+-- If asking for branch wise individual customer deposits.
+SELECT bname, cname, amount FROM deposit
+WHERE STR_TO_DATE(adate, '%d-%b-%Y') > '2006-01-06'
+ORDER BY bname;
+
+---- 15. Give branch wise loan of customer living in Nagpur.
+
+SELECT b.bname AS Branch, SUM(l.amount) AS Amount FROM customer c
+JOIN borrow l ON l.cname = c.cname
+JOIN branch b ON b.bname = l.bname
+WHERE c.city = "Nagpur"
+GROUP BY b.bname;
+
+---- 16. Count total no. of customers.
+
+SELECT city, COUNT(cname) FROM customer GROUP BY city;
+
+---- 17. Count total no. of depositor branch wise.
+
+SELECT bname AS Branch, COUNT(cname) AS Customers FROM deposit GROUP BY bname;
+
+-- For Verification
+SELECT city, COUNT(cname) FROM deposit d
+JOIN branch b ON b.bname = d.bname
+GROUP BY city;
+
+---- 18. Give maximum loan from branch VRCE.
+
+SELECT MAX(amount) FROM borrow WHERE bname = "VRCE";
+
+---- 19. Give living city wise loan of borrowers.
+
+SELECT c.city AS City, SUM(l.amount) AS Amount FROM borrow l
+JOIN customer c ON c.cname = l.cname
+GROUP BY c.city;
+
+---- 20. Give the number of customers who are depositor as well as borrowers.
+
+SELECT COUNT(l.cname) AS `Depositor as well as Borrower` FROM borrow l
+JOIN deposit d ON d.cname = l.cname;
+
+--
+----
+------ Group By & Having Clause
+----
+--
+
+---- 1. List the branches having sum of deposit more than 4000.
+
+SELECT bname, SUM(amount) FROM deposit
+GROUP BY bname
+HAVING SUM(amount) > 4000;
+
+---- 2. List the branches having a sum deposit more than 1000 and location in city Bombay.
+
+SELECT d.bname, SUM(d.amount) FROM deposit d
+JOIN branch b ON d.bname=b.bname
+WHERE b.city = "Bombay"
+GROUP BY bname
+HAVING SUM(amount) > 1000;
+
+---- 3. List the names of customers having deposit in the branches where the average deposit is more than 1000.
+
+SELECT DISTINCT d.cname, d.bname 
+FROM deposit d
+WHERE d.bname IN (
+    SELECT bname 
+    FROM deposit 
+    GROUP BY bname 
+    HAVING AVG(amount) > 1000
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
